e74774d4b4f9d06f23ba53b93d5a2b920251fd66
mafusuke <mafusukee@gmail.com>
2023-04-05 01:51:52 +0900

    Set TYPE to K2genmity

--- src/Enmity.h
+++ src/Enmity.h
@@ -8,6 +8,8 @@
 
 #define ENMITY_PATH [NSString stringWithFormat:@"%@/%@", NSHomeDirectory(), @"Documents/Enmity.js"]
 #define ENMITY_SOURCE [NSURL URLWithString:@"enmity"]
+#define VERSION @"2.1.9"
+#define TYPE @"K2genmity"
 
 // Disable logs in release mode
 # define NSLog(fmt, ... ) NSLog((@"[Enmity] " fmt), ##__VA_ARGS__);
